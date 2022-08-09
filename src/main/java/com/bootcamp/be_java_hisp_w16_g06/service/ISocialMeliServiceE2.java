ea4f32446c8e23e55867c13855ca0eec8c7019b7
JulianaOrtegaMeli <juliana.ortega@mercadolibre.com.co>
2022-08-09 16:50:33 -0500

    Add service E21 and E2

---
+++ src/main/java/com/bootcamp/be_java_hisp_w16_g06/service/ISocialMeliServiceE2.java
@@ -0,0 +1,12 @@
+package com.bootcamp.be_java_hisp_w16_g06.service;
+
+import com.bootcamp.be_java_hisp_w16_g06.dto.UserDTO;
+
+import java.util.List;
+
+public interface ISocialMeliServiceE2 {
+
+    List<UserDTO> userFollowers(Integer userId);
+
+    public List<UserDTO> findById(int userId);
+}
