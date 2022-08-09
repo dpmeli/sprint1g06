b465f861c3abeda8fcb91cfb1d4cbd649259398c
jhernandezan <juan.hernandezanzola@mercadolibre.com.co>
2022-08-09 13:57:34 -0500

    Add interface ISocialMeliServiceE3

---
+++ src/main/java/com/bootcamp/be_java_hisp_w16_g06/service/ISocialMeliServiceE3.java
@@ -0,0 +1,13 @@
+package com.bootcamp.be_java_hisp_w16_g06.service;
+
+import com.bootcamp.be_java_hisp_w16_g06.dto.RequestPostDTO;
+import com.bootcamp.be_java_hisp_w16_g06.dto.ResponsePostDTO;
+
+import java.util.List;
+
+public interface ISocialMeliServiceE3 {
+
+    void createPost(RequestPostDTO requestPostDTO);
+    List<ResponsePostDTO> getAllPost();
+
+}
--- src/main/java/com/bootcamp/be_java_hisp_w16_g06/service/SocialMeliServiceE3.java
+++ src/main/java/com/bootcamp/be_java_hisp_w16_g06/service/SocialMeliServiceE3.java
@@ -2,20 +2,26 @@ package com.bootcamp.be_java_hisp_w16_g06.service;
 
 import com.bootcamp.be_java_hisp_w16_g06.dto.RequestPostDTO;
 import com.bootcamp.be_java_hisp_w16_g06.dto.ResponsePostDTO;
+import com.bootcamp.be_java_hisp_w16_g06.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class SocialMeliServiceE3 {
+public class SocialMeliServiceE3 implements ISocialMeliServiceE3 {
 
-    List<ResponsePostDTO> responsePostDTOS;
+    @Autowired
+    PostRepository postRepository;
 
+    @Override
     public void createPost(RequestPostDTO requestPostDTO) {
 
     }
 
+    @Override
     public List<ResponsePostDTO> getAllPost() {
-        return responsePostDTOS;
+        return null;
     }
+
 }
