0e5cf40317a0c0b9eef8a732e9225417e707c21e
Tomas Octavio Rodriguez Herrera <tomrodriguez@co0y4yxy9wcqq.ml.com>
2022-08-18 12:33:41 -0500

    Test unitario con mock UnfollowUser - T-0002

--- src/test/java/testUnitario/service/SocialMeliServiceE1Tomas.java
+++ src/test/java/testUnitario/service/SocialMeliServiceE1Tomas.java
@@ -1,4 +1,80 @@
 package testUnitario.service;
 
+import com.bootcamp.be_java_hisp_w16_g06.dto.FollowIdDto;
+import com.bootcamp.be_java_hisp_w16_g06.dto.ResponseDTO;
+import com.bootcamp.be_java_hisp_w16_g06.entity.Follow;
+import com.bootcamp.be_java_hisp_w16_g06.entity.User;
+import com.bootcamp.be_java_hisp_w16_g06.exceptions.UserNotFoundException;
+import com.bootcamp.be_java_hisp_w16_g06.repository.UserFollowersRepository;
+import com.bootcamp.be_java_hisp_w16_g06.service.SocialMeliServiceE1;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.util.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
 public class SocialMeliServiceE1Tomas {
+
+    @Mock
+    UserFollowersRepository userFollowersRepository;
+
+    @InjectMocks
+    SocialMeliServiceE1 socialMeliServiceE1;
+
+    @Test
+    @DisplayName("El usuario a dejar de seguir NO existe")
+    public void unFollowUserNotFoundTest(){
+
+        //Arrange
+        List<User> listUser = new ArrayList<>();
+
+        FollowIdDto followIdDto = new FollowIdDto(7,9);
+
+        Mockito.when(userFollowersRepository.getUsersList()).thenReturn(listUser);
+
+        //Act //Assert
+        Assertions.assertThrows(UserNotFoundException.class, () -> socialMeliServiceE1.unFollowUser(followIdDto));
+
+    }
+
+    @Test
+    @DisplayName("El usuario a dejar de seguir SI existe")
+    public void unFollowUserFoundTest() {
+
+        //Arrange
+        ResponseDTO responseDTOExpected = new ResponseDTO("Unfollow Successful", 200);
+        List<User> listUser = new ArrayList<>();
+
+        List<Follow> listFollower = new ArrayList<>();
+        List<Follow> listFollowed = new ArrayList<>();
+
+        Follow follow = new Follow(7, "Juan");
+        listFollowed.add(follow);
+
+        User userOne = new User(3, "Tomas", listFollowed, listFollower);
+        User userTwo = new User(7, "Juan", listFollower, listFollowed);
+
+        FollowIdDto followIdDto = new FollowIdDto(3,7);
+
+        listUser.add(userOne);
+        listUser.add(userTwo);
+
+        Mockito.when(userFollowersRepository.getUsersList()).thenReturn(listUser);
+
+        //Act
+        ResponseDTO responseDTOService;
+        responseDTOService = socialMeliServiceE1.unFollowUser(followIdDto);
+
+        //Assert
+        Assertions.assertEquals(responseDTOExpected, responseDTOService);
+    }
+
 }
