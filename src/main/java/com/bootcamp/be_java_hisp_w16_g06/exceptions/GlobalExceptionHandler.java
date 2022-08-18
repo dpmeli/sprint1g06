0a342e3efabb0a18b6da362b23ee1b20baa07c43
Tomas Octavio Rodriguez Herrera <tomrodriguez@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2022-08-18 10:10:34 -0500

    Add dependency starter-validation and DTO Message Error

--- src/main/java/com/bootcamp/be_java_hisp_w16_g06/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/bootcamp/be_java_hisp_w16_g06/exceptions/GlobalExceptionHandler.java
@@ -1,13 +1,19 @@
 package com.bootcamp.be_java_hisp_w16_g06.exceptions;
 
+import com.bootcamp.be_java_hisp_w16_g06.dto.MessageDTO;
 import com.bootcamp.be_java_hisp_w16_g06.dto.ResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends RuntimeException{
 
     @ExceptionHandler(UserNotFoundException.class)
 
@@ -32,6 +38,34 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ResponseDTO> catchFollowException(FollowException e) {
         ResponseDTO exceptionResponse = new ResponseDTO(e.getMessage(), 400);
         return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ResponseEntity<MessageDTO> validatingDataTypes(
+            MethodArgumentNotValidException exception
+    ) {
+        MessageDTO messageDTO = new MessageDTO();
+        messageDTO.setStatusCode(HttpStatus.BAD_REQUEST.value());
+
+        HashMap<String, List<String>> errors = new HashMap<>();
+
+        exception
+                .getFieldErrors()
+                .forEach(
+                        e -> {
+                            String field = e.getField();
+                            String msg = e.getDefaultMessage();
+                            List<String> errorFields = new ArrayList<>();
+                            if (errors.containsKey(field)){
+                                errorFields = errors.get(field);
+                            }
+                            errorFields.add(msg);
+                            errors.put(field, errorFields);
+                        }
+                );
+        messageDTO.setErrorField(errors);
+        return new ResponseEntity<>(messageDTO, HttpStatus.BAD_REQUEST);
     }
 
 }
