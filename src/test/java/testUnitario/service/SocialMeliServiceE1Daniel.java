8f167f381f270d7ef0913e370dc003e040f3fd85
dpmeli <daniel.patino@mercadolibre.com.co>
2022-08-18 13:48:03 -0500

    Aumento de covertura Service 1

--- src/test/java/testUnitario/service/SocialMeliServiceE1Daniel.java
+++ src/test/java/testUnitario/service/SocialMeliServiceE1Daniel.java
@@ -4,6 +4,7 @@ import com.bootcamp.be_java_hisp_w16_g06.dto.FollowIdDto;
 import com.bootcamp.be_java_hisp_w16_g06.dto.ResponseDTO;
 import com.bootcamp.be_java_hisp_w16_g06.entity.Follow;
 import com.bootcamp.be_java_hisp_w16_g06.entity.User;
+import com.bootcamp.be_java_hisp_w16_g06.exceptions.FollowException;
 import com.bootcamp.be_java_hisp_w16_g06.exceptions.UserNotFoundException;
 import com.bootcamp.be_java_hisp_w16_g06.repository.UserFollowersRepository;
 import com.bootcamp.be_java_hisp_w16_g06.service.SocialMeliServiceE1;
@@ -39,6 +40,34 @@ public class SocialMeliServiceE1Daniel {
         //Act
 
         Assertions.assertThrows(UserNotFoundException.class, () -> socialMeliServiceE1.followUser(idDto));
+    }
+
+    @Test
+    public void followServiceTest_ALREADY_FOLLOWING() {
+        //Arrange
+        ResponseDTO responseDTOExpected = new ResponseDTO("Follow Successful", 200);
+        List<User> listUser = new ArrayList<>();
+
+        List<Follow> listFollower = new ArrayList<>();
+        List<Follow> listFollowed = new ArrayList<>();
+
+        Follow follow = new Follow(3, "Juan");
+        listFollowed.add(follow);
+
+        User userOne = new User(3, "Tomas", listFollowed, listFollower);
+        User userTwo = new User(7, "Juan", listFollower, listFollowed);
+
+        FollowIdDto followIdDto = new FollowIdDto(3, 7);
+
+        listUser.add(userOne);
+        listUser.add(userTwo);
+
+        Mockito.when(userFollowersRepository.getUsersList()).thenReturn(listUser);
+
+        //Act
+
+        //Assert
+        Assertions.assertThrows(FollowException.class, () -> socialMeliServiceE1.followUser(followIdDto));
     }
 
     @Test
