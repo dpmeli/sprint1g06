bbd20cbdd363d8f1ad7c981ad0a84d04c440abbd
Tomas Octavio Rodriguez Herrera <tomrodriguez@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2022-08-19 08:00:21 -0500

    Add test unitario controller unFollow US007

--- src/test/java/testUnitario/controller/SocialMeliControllerE1.java
+++ src/test/java/testUnitario/controller/SocialMeliControllerE1.java
@@ -1,10 +1,15 @@
 package testUnitario.controller;
 
 import com.bootcamp.be_java_hisp_w16_g06.controller.SocialMeliController;
+import com.bootcamp.be_java_hisp_w16_g06.dto.FollowIdDto;
 import com.bootcamp.be_java_hisp_w16_g06.dto.ResponseDTO;
+import com.bootcamp.be_java_hisp_w16_g06.service.ISocialMeliServiceE1;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +20,9 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class SocialMeliControllerE1 {
+
+    @Mock
+    ISocialMeliServiceE1 socialMeliServiceE1;
 
     @InjectMocks
     SocialMeliController socialMeliController;
@@ -31,5 +39,26 @@ public class SocialMeliControllerE1 {
         //System.out.println(response);
 
         assertEquals(HttpStatus.OK, socialMeliController.test(statusCodeTest).getStatusCode());
+    }
+
+
+    @Test
+    @DisplayName("US007")
+    public void unFollowTest(){
+
+        int userId = 5;
+        int userIdToUnfollow = 7;
+
+        FollowIdDto followIdDto = new FollowIdDto(userId, userIdToUnfollow);
+
+        ResponseDTO responseDTO = new ResponseDTO();
+        responseDTO.setMessage("unfollow successfull");
+        responseDTO.setStatusCode(HttpStatus.OK.value());
+
+        when(socialMeliServiceE1.unFollowUser(followIdDto)).thenReturn(responseDTO);
+
+        Assertions.assertEquals(HttpStatus.OK, socialMeliController.US007(userId, userIdToUnfollow).getStatusCode());
+
+
     }
 }
